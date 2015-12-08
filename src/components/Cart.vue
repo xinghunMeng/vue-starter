b9bc440edd2f64210b438cb48428e21752e3f090
luankefei <luankefei@gmail.com>
2015-12-08 22:26:42 +0800

    购物车样式

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cart">
-    
+    <span class='price'>￥76</span>
+    <span class='desc'>(含2元配送)</span>
+    <button class='submit'>选好了</button>
   </div>
 </template>
 
@@ -17,5 +19,26 @@
 
 <style lang='stylus'>
 
+.cart
+  height: 44px
+  background: #333
+  line-height: 44px
+  
+  .price
+    margin-left: 15px
+    font-size: (18 / 14)rem
+    color: #fff
+
+  .desc
+    font-size: (12/ 14)rem
+    color: #fff
+
+  .submit
+    float: right
+    background: #ffe600
+    border: none
+    width: 130px
+    text-align: center
+    font-size: (16 / 14)rem
 
 </style>
